320fee93b9d3622e1cb44e6535c9f63c1e94646c
Vasif Mammadov <vasif.will@gmail.com>
2022-07-29 09:49:59 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import JSONDATA from "./MOCK_DATA.json"
 
 function App() {
+  const[searchTerm, setSearchTerm] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input placeholder='search...' onChange={(event) => setSearchTerm(event.target.value)}/>
+    <table >
+        <tbody>
+         <tr>
+            <th> First Name</th>
+            <th> Last Name</th>
+            <th> Email</th>
+            <th> Gender</th>
+          </tr>
+          {JSONDATA.filter((val) => {
+            if( searchTerm === "") {
+              return val
+              } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase()))
+              { return val
+              }}).map((val, key) => {
+                return (
+                  <tr key={key}>
+                      <td>{val.first_name}</td>
+                      <td>{val.last_name}</td>
+                      <td>{val.gender}</td>
+                      <td>{val.email}</td>
+                  </tr>
+                  )})}
+          </tbody>
+      </table>
+
+    
+     
     </div>
   );
 }
-
+ 
 export default App;
